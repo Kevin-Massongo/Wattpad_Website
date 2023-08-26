7f817be0a5598ad3d07fe832c51dade01d854070
= <kevinmassongo196@gmail.com>
2023-08-27 01:09:08 +0200

    feat: add code

--- index.js
+++ index.js
@@ -4,5 +4,14 @@ const menuBrowse = document.querySelector('.menuBrowser');
 openBrowse.addEventListener('click', addBrowse)
 
 function addBrowse(){
-    menuBrowse.classList.toggle('open')
+    menuBrowse.classList.toggle('open');
+}
+
+const openCommunity = document.querySelector('.community')
+const menuCommunity = document.querySelector('.menuCommunity')
+
+openCommunity.addEventListener('click', addCommunity)
+
+function addCommunity(){
+    menuCommunity.classList.toggle('open')
 }
